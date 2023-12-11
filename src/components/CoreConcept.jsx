a10eae32655c31cf466ee19d80792d535d8c9d81
aserra <alexandreapserra@hotmail.com>
2023-12-11 10:36:25 -0300

    Atualização do projeto

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,10 +1,9 @@
-export default function CoreConcept ({image,title,description}) {
-    return( 
+export default function CoreConcept({ image, title, description }) {
+  return (
     <li>
-      <img src={image}
-       alt={title} />
-       <h3>{title}</h3>
-       <p>{description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
-    )
-  }
+  );
+}
